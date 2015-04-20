1a0326b9536790d51fea3e286afc80b0296d10ba
Leeingnyo <dyd1928@naver.com>
2015-04-20 19:54:47 +0900

    test login failure

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -36,4 +36,10 @@ class UsersControllerTest < ActionController::TestCase
     assert json['user_name'] == 'Romeo'
     assert json['login_count'] == 101
   end
+
+  test '#login_failure' do
+    post :login, {'username' => 'Shrek', 'password' => 'ilovefiona'}
+    json = JSON.parse(response.body)
+    assert json['error_code'] == -4
+  end
 end
