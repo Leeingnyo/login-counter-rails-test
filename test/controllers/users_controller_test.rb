3f51307a58260823ecf55e867383eb7305c99d73
Leeingnyo <dyd1928@naver.com>
2015-04-20 19:44:45 +0900

    test signup failure duplicate

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -12,4 +12,15 @@ class UsersControllerTest < ActionController::TestCase
     json = JSON.parse(response.body)
     assert json['error_code'] == -2
   end
+  
+  test '#signup_failure_dup' do
+    post :signup, {'username' => 'testuser', 'password' => 'testpass'}
+    json = JSON.parse(response.body)
+    assert json['user_name'] == 'testuser'
+    assert json['login_count'] == 1
+
+    post :signup, {'username' => 'testuser', 'password' => 'testpass'}
+    json = JSON.parse(response.body)
+    assert json['error_code'] == -3
+  end
 end
